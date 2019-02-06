b8f4ae69f68445bc575a4e6935be6bd582f45fc4
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-06 07:19:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   temp_array = string.split(" ")
   reversed_array = []
-  temp_array.collect do |x| reversed_array << temp_string.shift
+  temp_array.collect do |x| reversed_array << temp_array.shift
     reversed_array = reversed_array.reverse
   end
   sent = reversed_array.join(' ')
